0376ab51ab134c071e017b0900535bf2f3f58b4b
kartike rohila 37 <kartikerohila37@gmail.com>
2026-07-03 17:15:36 +0530

    .

--- practice_python/chapter8/problem4.py
+++ practice_python/chapter8/problem4.py
@@ -1,7 +1,7 @@
-def pattern(n):
-    if (n==0):
-        return 
-    print("*"*n)
-    pattern(n-1)
+# def pattern(n):
+#     if (n==0):
+#         return 
+#     print("*"*n)
+#     pattern(n-1)
 
-pattern(5)
+# pattern(5)
---
+++ practice_python/chapter8/problem9.py
@@ -0,0 +1,3 @@
+
+from urllib.parse import quote_plus
+print(quote_plus("11aA2#bB"))
